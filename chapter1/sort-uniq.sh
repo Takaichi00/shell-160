d36ba6e62a9e9154766a9f9ae7a26ea4d093d9c2
Tomoaki Takaichi <kiruhihhohu555@gmail.com>
2022-01-08 23:55:23 +0900

    chapter1.3: 別解

--- chapter1/sort-uniq.sh
+++ chapter1/sort-uniq.sh
@@ -2,3 +2,4 @@
 
 seq 5 | awk '{print $1%2? "奇数":"偶数"}'
 seq 5 | awk '{print $1%2? "奇数":"偶数"}' | sort | uniq -c | awk '{print $2,$1}'
+seq 5 | awk '{print $1%2 ? "奇数":"偶数"}' | awk '{a[$1]++}END{for(k in a)print k,a[k]}'
