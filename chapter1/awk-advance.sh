9dea2fb04fee2ecfa1774c9e470895b9a9aea115
Tomoaki Takaichi <kiruhihhohu555@gmail.com>
2022-01-21 23:19:54 +0900

    chapter1.4: q6 another

--- chapter1/awk-advance.sh
+++ chapter1/awk-advance.sh
@@ -3,4 +3,5 @@
 cat shellgei160/qdata/5/ntp.conf | awk '$1=="pool"'
 cat shellgei160/qdata/5/ntp.conf | awk '$1=="pool"' | awk '{print $2}'
 seq 5 | awk '{for(i=1;i<$1;i++){printf " "};print "x"}' | tac
+echo -e "    x\n   x\n  x\n x\nx"
 
