93e06dfa5422ee483f14b9bb2c83315c7780b9dd
TomoakiTakaichi <kiruhihhohu555@gmail.com>
2022-01-27 23:43:15 +0900

    chapter1.6: Q7-4

--- chapter1/kakeibo.sh
+++ chapter1/kakeibo.sh
@@ -3,4 +3,5 @@
 cat kakeibo.txt | awk '{tax=1.1; print $0,tax}'
 cat kakeibo.txt | awk '{tax=($1<"20191001"||$2~"^*") ? 1.08:1.1;print $0,tax}'
 cat kakeibo.txt | awk '{tax=($1<"20191001"||$2~"^*") ? 1.08:1.1;print $0,tax}' | awk '{print int($3*$4)}'
+cat kakeibo.txt | awk '{tax=($1<"20191001"||$2~"^*") ? 1.08:1.1;print $0,tax}' | awk '{print int($3*$4)}' | numsum
 
