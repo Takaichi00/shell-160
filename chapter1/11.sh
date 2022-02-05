09b71954212f9108ede1eb1aa71f3f7292c3d416
TomoakiTakaichi <kiruhihhohu555@gmail.com>
2022-02-06 01:55:15 +0900

    chapter1: 11-4

--- chapter1/11.sh
+++ chapter1/11.sh
@@ -2,3 +2,5 @@ cat gijiroku.txt | xargs -n2
 cat gijiroku.txt | xargs -n2 | sed 's/^すず/鈴木/'
 cat gijiroku.txt | xargs -n2 | sed 's/^すず/鈴木/;s/さと/佐藤/'
 
+cat gijiroku.txt | xargs -n2 | sed 's/^すず/鈴木/;s/さと/佐藤/;s/^やま/山田/;s/ /:/'
+
